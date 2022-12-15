7ae07fc823b6fa8fa4ba3528ee99b9a74fed5151
UrielLevy1 <your_github_email>
2022-12-15 19:02:45 +0200

    STATIC_ROOT comment for heroku

--- library_f/settings.py
+++ library_f/settings.py
@@ -264,6 +264,6 @@ LOGIN_REDIRECT_URL='/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
